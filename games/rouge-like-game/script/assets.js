86a0be28098b6e861e678c45f7980e538568f199
KetCodeStash <ketoffacc@gmail.com>
2025-05-13 16:29:02 +0700

    bullet function

--- games/rouge-like-game/script/assets.js
+++ games/rouge-like-game/script/assets.js
@@ -1,6 +1,7 @@
 
 const assetLoad = {
-    plr: document.getElementById('triangle')
+    plr: document.getElementsByClassName('triangle')[0]
 }
+console.log(assetLoad.plr)
 
 export default assetLoad;
--- games/rouge-like-game/script/handler.js
+++ games/rouge-like-game/script/handler.js
@@ -24,30 +24,65 @@ function updatePlrAngle() {
     // console.log(gameVal.PlrX+'-'+gameVal.MouseX);
 
     const deg = (Math.atan2(pointY, pointX)/Math.PI)*180;
-    console.log(deg)
+    // console.log(deg)
     assets.plr.style.transform = `rotate(${deg}deg)`;
 }
-function spawnBullet() {
+function createNewElement(className){
     const parent = document.getElementById('parent');
     const bullet = document.createElement("div");
-    bullet.setAttribute("id", "bullet")
-    bullet.style.top = gameVal.PlrY+25+"px";
-    bullet.style.left = gameVal.PlrX+20+"px";
+    bullet.setAttribute("class", className)
     parent.appendChild(bullet);
+    return bullet;
+}
+function calcBulletDirectionsSpeed(){
+    let pointX = gameVal.PlrX-gameVal.MouseX;
+    let pointY = gameVal.PlrY-gameVal.MouseY;
+    if(pointX<pointY){
+        pointY/=pointY;
+        pointX/=pointY;
+    }else{
+        pointX/=pointX;
+        pointY/=pointX;
+    }
+    console.log(pointX+', '+pointY)
+    return {
+        x : pointX,
+        y : pointY
+    }
+}
 
-    const lifespan = createAttribute('lifespan')
-    lifespan.value = '5000';
-    bullet.setAttribute(lifespan);
+function spawnBullet() {
+    const bullet = createNewElement('bullet')
+
+    const directionSpeed = calcBulletDirectionsSpeed()
+    
+    const xSpd = document.createAttribute('xSpd')
+    xSpd.value = String(directionSpeed['x']) 
+    const ySpd = document.createAttribute('ySpd')
+    ySpd.value = String(directionSpeed['y'])
+    const lifespan = document.createAttribute('lifespan') 
+    lifespan.value = '500';
+
+    bullet.setAttributeNode(lifespan);
+    bullet.setAttributeNode(xSpd);
+    bullet.setAttributeNode(ySpd);
 }
 function updateBullets() {
-    const bulletsElm = document.getElementById('bullet')
+    const bulletsElm = document.getElementsByClassName('bullet')
     const bullets = [...bulletsElm];
-    bulletsElm.forEach(bullet => {
+    bullets.forEach(bullet => {
         const lifespan = Number(bullet.getAttribute('lifespan'))
         if (lifespan<=0) {
             bullet.remove()
             return
-        } 
+        }
+        const xSpd = Number(bullet.getAttribute('xSpd'))
+        const ySpd = Number(bullet.getAttribute('ySpd'))
+        const xPos = bullet.offsetLeft-xSpd;
+        const yPos = bullet.offsetTop-ySpd;
+        bullet.style.left = `${xPos}px`
+        bullet.style.top = `${yPos}px`
+        // console.log(xPos+', '+yPos)
         bullet.setAttribute('lifespan', String(lifespan-1));
     });
 }
